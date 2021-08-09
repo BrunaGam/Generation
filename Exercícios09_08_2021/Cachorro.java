ad6acc0829c0f6e12ef84c77a376070b17f8e9b6
Bruna Amoretti Guimarães <brubru.amoretti@gmail.com>
2021-08-09 18:54:52 -0300

    Pasta criada dia 09/08/2021 para armazenar os exercícios da Generation

---
+++ Exercícios09_08_2021/Cachorro.java
@@ -0,0 +1,11 @@
+package AtividadesGeneration05;
+
+public class Cachorro extends Animal {
+
+	public void somAnimal() {
+		System.out.println("Som do Cachorro: Au au au...");
+	}
+	public void correr() {
+		System.out.println("Cachorro correndo...");
+	}
+}
---
+++ Exercícios09_08_2021/TestaAnimal.java
@@ -0,0 +1,21 @@
+package AtividadesGeneration05;
+
+public class TestaAnimal {
+	
+	public void somAnimal(Animal animal) {
+		animal.somAnimal();
+	}
+    public void correr(Animal animal) {
+	    animal.correr();
+	}
+    public void subir(Animal animal) {
+    	animal.subir();
+    }
+	
+    public void idade(Animal animal) {
+    	animal.setIdade(20);
+    }
+    public void nome(Animal animal) {
+    	animal.getNome();
+    }
+}
